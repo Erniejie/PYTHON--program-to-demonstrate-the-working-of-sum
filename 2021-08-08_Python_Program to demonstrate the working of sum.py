96c2c4873e58ab53c639a9c681445904d6ba7844
erniejie <46618858+Erniejie@users.noreply.github.com>
2021-08-08 05:19:31 +0000

    Add files via upload

---
+++ 2021-08-08_Python_Program to demonstrate the working of sum.py
@@ -0,0 +1,14 @@
+#Python program to demonstrate the working of sum
+#Computer Programming Tutor,June 11 2021
+
+no = [5,6,4,7,5,9,12,50]
+
+# Starting Parameters is not Provided
+
+Sum = sum(no)
+print(Sum)
+
+#start =10
+Sum= sum(no,40)
+
+print(Sum)
